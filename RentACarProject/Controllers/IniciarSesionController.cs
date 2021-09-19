c545bbc999f25efa9e29f25340299a8ff61c8f9f
alfredo Torres <alfredotorres.niu@gmail.com>
2021-09-19 17:02:17 -0600

    Agregar archivos de proyecto.

---
+++ RentACarProject/Controllers/IniciarSesionController.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace RentACarProject.Controllers
+{
+    public class IniciarSesionController : Controller
+    {
+        // GET: IniciarSesion
+        public ActionResult IniciarSesion()
+        {
+            return View();
+        }
+
+        public ActionResult Registrarse()
+        {
+            return View();
+        }
+    }
+}
