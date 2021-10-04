37da271adc384391f0a04a1e778c5ea7579ed2e8
spyro0027 <proyectb2.1@gmail.com>
2021-10-03 20:40:30 -0600

    Se agregó navbar

---
+++ RentACarProject/Scripts/navbar.js
@@ -0,0 +1,13 @@
+/* Please ❤ this if you like it! */
+(function($) {
+    //Menu On Hover
+    $('body').on('mouseenter mouseleave', '.nav-item', function(e) {
+        if ($(window).width() > 750) {
+            var _d = $(e.target).closest('.nav-item');
+            _d.addClass('show');
+            setTimeout(function() {
+                _d[_d.is(':hover') ? 'addClass' : 'removeClass']('show');
+            }, 1);
+        }
+    });
+})(jQuery);
