f133f8c2d890cd451204fa3eef2267247bcf11e7
Veronica122 <Veronicarodriguez.niu@gmail.com>
2021-10-04 10:14:08 -0600

    Se agregó vista DetalleAutos

---
+++ RentACarProject/Controllers/AutosController.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace RentACarProject.Controllers
+{
+    public class AutosController : Controller
+    {
+        // GET: Autos
+        public ActionResult Index()
+        {
+            return View();
+        }
+        // GET: DetalleAutos
+        public ActionResult DetalleAutos()
+        {
+            return View();
+        }
+    }
+}
