b69961c96e3e4260cb86d6b271e0cb43f8856309
ana silvia lopez mendez <silvialolpez.niu@nehemiahiu.com>
2021-10-07 17:29:31 -0600

    Atributos en la clase Clientes

--- DAL/Models/Clientes.cs
+++ DAL/Models/Clientes.cs
@@ -19,27 +19,40 @@ namespace DAL.Models
         [Key]
         public int IdCliente { get; set; }
 
-        [Required]
+        [Required(ErrorMessage = "El campo no puede estar vacio")]
         [StringLength(55)]
         public string NombreCliente { get; set; }
 
-        [Required]
+
+        [Required(ErrorMessage = "El campo no puede estar vacio")]
         [StringLength(55)]
         public string Apellido { get; set; }
 
-        [Required]
+
+        [Required(ErrorMessage = "El campo no puede estar vacio")]
+        [EmailAddress(ErrorMessage ="No es una direccion de correo Electronico")]
         [StringLength(55)]
         public string Correo { get; set; }
 
-        [Required]
+
+        [Required(ErrorMessage = "El campo no puede estar vacio")]
         [StringLength(55)]
         public string Contra { get; set; }
 
-        public int Edad { get; set; }
+        [NotMapped]
+        [Compare("Contra")]
+        [Required(ErrorMessage = "El campo no puede estar vacio")]
+        [StringLength(55)]
+        public string ConfirmarPass { get; set; }
 
+        [Range(17, int.MaxValue, ErrorMessage = "Cantidad no válida")]
+        [Required(ErrorMessage = "El campo no puede estar vacio")] public int Edad { get; set; }
+
+        [DataType(DataType.PhoneNumber)]
+        [RegularExpression(@"^\(?([0-8]{2})\)?[-.]?([0-8]{2})[-.]?[-.]?([0-8]{4})$", ErrorMessage = "Error el numero debe tener 8 digitos")] 
         public int Telefono { get; set; }
 
-        public int Rol { get; set; }
+        public Roles Rol { get; set; }
 
         [StringLength(20)]
         public string Estado { get; set; }
