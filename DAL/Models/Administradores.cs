d16865cc70dfd3320b5d3662fcc6580f9d40f518
ana silvia lopez mendez <silvialolpez.niu@nehemiahiu.com>
2021-10-12 08:43:05 -0600

    Crud Admins

--- DAL/Models/Administradores.cs
+++ DAL/Models/Administradores.cs
@@ -11,26 +11,34 @@ namespace DAL.Models
         [Key]
         public int IdAdministrador { get; set; }
 
-        [Required]
+        [Required(ErrorMessage = "El campo no puede estar vacio")]
         [StringLength(55)]
         public string Nombre { get; set; }
 
-        [Required]
+        [Required(ErrorMessage = "El campo no puede estar vacio")]
         [StringLength(55)]
         public string Apellido { get; set; }
 
-        [Required]
+        [Required(ErrorMessage = "El campo no puede estar vacio")]
         [StringLength(55)]
+        [EmailAddress(ErrorMessage = "No es una direccion de correo Electronico")]
+
         public string Correo { get; set; }
 
-        [Required]
+        [Required(ErrorMessage = "El campo no puede estar vacio")]
         [StringLength(55)]
         public string Contra { get; set; }
 
+        [Required(ErrorMessage = "El campo no puede estar vacio")]
+        [Range(17, int.MaxValue, ErrorMessage = "Cantidad no válida")]
         public int Edad { get; set; }
 
+        [Required(ErrorMessage = "El campo no puede estar vacio")]
+        [DataType(DataType.PhoneNumber)]
+        [RegularExpression(@"^\(?([0-8]{2})\)?[-.]?([0-8]{2})[-.]?[-.]?([0-8]{4})$", ErrorMessage = "Error el numero debe tener 8 digitos")]
         public int Telefono { get; set; }
 
-        public int Rol { get; set; }
+
+        public Roles Rol { get; set; }
     }
 }
--- RentACarProject/Controllers/AdministradoresController.cs
+++ RentACarProject/Controllers/AdministradoresController.cs
@@ -1,4 +1,7 @@
-﻿using System;
+﻿using BAL.Iservices;
+using BAL.Services;
+using DAL.Models;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
@@ -9,9 +12,53 @@ namespace RentACarProject.Controllers
     public class AdministradoresController : Controller
     {
         // GET: Administradores
-        public ActionResult MostrarAdministradores()
-        {
-            return View();
+        
+            Contexto contexto = new Contexto();
+
+            private IAdministradoresRepository administradoresRepository;
+
+            public AdministradoresController()
+            {
+                this.administradoresRepository = new AdministradoresRepository(new Contexto());
+            }
+
+            public ActionResult MostrarAdministradores()
+            {
+                var model = contexto.Administradores.ToList();
+
+                return View(model);
+            }
+            public ActionResult AddOrEdit(int IdAdmin = 0)
+            {
+                var model = new Administradores();
+
+                if (IdAdmin > 0)
+                {
+                    model = administradoresRepository.ObtenerId(IdAdmin);
+                }
+                return View(model);
+            }
+            [HttpPost]
+            public ActionResult AddOrEdit(Administradores model)
+            {
+
+                if (ModelState.IsValid)
+                {
+                    model.Rol = Roles.admin;
+                    var hecho = administradoresRepository.AgregarEditar(model);
+                    if (hecho)
+                    {
+                        return RedirectToAction("MostrarAdministradores");
+                    }
+                }
+                return View(model);
+            }
+
+            public ActionResult EliminarAdmin(int IdAdmin)
+            {
+                var hecho = administradoresRepository.EliminarAdmin(IdAdmin);
+
+                return RedirectToAction("MostrarAdministradores");
+            }
         }
     }
-}
