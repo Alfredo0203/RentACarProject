9d246bd1786c0bd9f59eb4b9813213d2d9098094
Alejandra <alejandraramirez.niu@nehemiahiu.com>
2021-10-19 21:37:28 -0600

    Tabla Compras y Alquiler

--- RentACarProject/Controllers/DetalleComprasController.cs
+++ RentACarProject/Controllers/DetalleComprasController.cs
@@ -1,5 +1,7 @@
-﻿using System;
+﻿using DAL.Models;
+using System;
 using System.Collections.Generic;
+using System.Data.Entity;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
@@ -8,10 +10,18 @@ namespace RentACarProject.Controllers
 {
     public class DetalleComprasController : Controller
     {
+
+        Contexto contexto = new Contexto();
+
         // GET: DetalleCompras
         public ActionResult DetalleCompras()
         {
-            return View();
+            var compras = contexto.DetalleCompras.Include(x => x.Compras).ToList();
+            var proveedores = contexto.Compras.Include(x => x.Proveedores).ToList();
+
+            var lista = contexto.DetalleCompras.ToList();
+            return View(lista);
         }
+
     }
 }
--- RentACarProject/Controllers/HistorialAlquilerController.cs
+++ RentACarProject/Controllers/HistorialAlquilerController.cs
@@ -1,5 +1,7 @@
-﻿using System;
+﻿using DAL.Models;
+using System;
 using System.Collections.Generic;
+using System.Data.Entity;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
@@ -8,10 +10,18 @@ namespace RentACarProject.Controllers
 {
     public class HistorialAlquilerController : Controller
     {
+        Contexto contexto = new Contexto();
+
         // GET: HistorialAlquiler
         public ActionResult HistorialAlquiler()
         {
-            return View();
+            //var compras = contexto.DetalleCompras.Include(x => x.Compras).ToList();
+
+            var clientes = contexto.HistorialAlquiler.Include(x => x.Clientes).ToList();
+            var auto = contexto.HistorialAlquiler.Include(x => x.Autos).ToList();
+
+            var model = contexto.HistorialAlquiler.ToList();
+            return View(model);
         }
     }
 }
